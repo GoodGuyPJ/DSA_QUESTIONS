cd7df57ea3e2050784077596f430ebb040a9a739
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-05-13 23:35:20 +0530

    stack

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -679,3 +679,24 @@ public:
 }
 
 // Next Greater Element
+ class solution{
+    public: 
+    vector<int> nextGreaterElements(vector<int> & nums){
+        int n = nums.size();
+        vector<int nge(n, -1);
+        stack<int> st;
+        for(int i= 2*n-1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
+                st.pop();
+            }
+            if(i<n){
+                if(!st.empty()) nge[i] = st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        return nge;
+    }
+ }
+
+
+//  Next Greater Element - II 
