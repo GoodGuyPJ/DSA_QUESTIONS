ec2e3bfacf72c2cc60788f430cc3aacdb8695901
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 16:50:49 +0530

    Number of Provinces

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -49,7 +49,41 @@ void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls){
 TC -> O(N) + O(2*Edges)
 SC-> O(N)
 
-// 3
+// 3 Number of Provinces
+
+class Solution {
+public:
+    int n;
+    void dfs(vector<vector<int>> &adj, int u, vector<bool>& visited) {
+        visited[u] = true;
+        
+        //Visit neighbours
+        for(int v = 0; v < n; v++) {
+            if(adj[u][v] == 1 && !visited[v]) {
+                dfs(adj, v, visited);
+            }
+        }
+    }
+    
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+        
+        vector<bool> visited(n, false);
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(isConnected, i, visited);
+            }
+        }
+        
+        return count;
+        
+    }
+};
+
 
 
 
