c1b6687717093444f76bafb9b3dfe44d02705aa8
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 15:15:48 +0530

    BFS

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,4 +1,24 @@
-// 1
+// 1 - BFS traversal
+vector<int> bfs( int V, vector<int> adj[]){
+  itn vis[V] = {0};
+  vis[0] = 1;
+  queue<int> q;
+  q.push(0);
+  vector<int> bfs;
+  while(!q.empty()){
+    int node = q.front();
+    q.pop();
+
+    bfs.push_back(node);
+    for(auto it : adj[node]){
+      if(!vis[it]){
+        vis[it] =1;
+        q.push(it);
+      }
+    }
+  }
+  return bfs;
+}
 
 
 
