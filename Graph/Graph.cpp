8b1bd2d95f21ba9d4df5322330469729199727ab
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 15:34:23 +0530

    DFS Graph

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -19,12 +19,35 @@ vector<int> bfs( int V, vector<int> adj[]){
   }
   return bfs;
 }
+Tc-> O(N) + O(2*Edges)
+SC- O(N)
 
 
+// 2 DFS
+class Solution{
+  public:
+void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls){
+  vis[node] = 1;
+  ls.push_back(node);
+  for(auto it: adj[node]){
+    if(!vis[it]){
+      dfs(it, adj, vis, ls);
+    }
+  }
+}
+  vector<int> dfsofGraph(int V, vector<int adj[]){
+    int vis[V] = {0};
+    int start = 0;
+    vector<int> ls;
 
-// 2
+    dfs(start, adj, vis, ls);
 
+    return ls;
+  }
+};
 
+TC -> O(N) + O(2*Edges)
+SC-> O(N)
 
 // 3
 
