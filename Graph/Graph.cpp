d1d86e422d9cf59cce297f313399d4ec2464c607
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 17:43:35 +0530

    Number of Islands

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -87,7 +87,52 @@ public:
 
 
 
-// 4
+// 4 Number of Islands
+ class Solution{
+  public:
+  void bfs( int row, int col, vector<vector<int>>& vis, vector<vector<char>> grid){
+    vis[row][col] = 1;
+    queue<pair<int, int>> q;
+    q.push({row, col});
+
+    int n = grid.size(), m = grid[0].size();
+    while(!q.empty()){
+      int row = q.fornt().first;
+      int col = q.front().second;
+      q.pop();
+
+      for(int delRow = -1; delRow <= 1; delRow++){
+        for(int delCol = -1; delCol<=1; delCol++){
+          int nRow = row + delRow;
+          int nCol = col + delCol;
+
+          if(nRow>=0 && nRow<n && nCol>=0 && nCol<n && !vis[nRow][nCol] && grid[nRow][nCol] == '1' ){
+            vis[nRow][nCol] = 1;
+            q.push({nRow, nCol});
+          }
+        }
+      }
+    }
+  }
+
+  int numIsLands(vector<vector<char>> & grid){
+    int n = grid.size();
+    int m = grid[0].size();
+
+    vector<vector<int>> vis(n, vis(m, 0));
+    itn cnt = 0;
+    for(int row = 0; row < n; row++){
+      for(int col = 0; col <m; col++){
+        if(!vis[row][col] && grid[row][col] == '1'){
+            cnt++;
+            bfs(row, col, vis, grid);
+        }
+      }
+    }
+    return cnt;
+  }
+
+ };
 
 
 
