58c0819382c273e925c5e2987eda4cee58793e48
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-09-10 23:20:31 +0530

    Diameter of BST Tree

--- BinarySearchTree/QuestionInterview.cpp
+++ BinarySearchTree/QuestionInterview.cpp
@@ -12,3 +12,15 @@ int height(struct Node* node){
 }
 
 //Diameter of Tree
+int diameter(Node* root){
+  if(root == nullptr){
+    return 0;
+  }
+
+  int op1 = diameter(root->left);
+  int op2 = diameter(root->right);
+  int op3 = height(root->left) + height(root->right) + 1;
+
+  int ans = max(op1, max(op2, op3));
+  return ans;
+}
