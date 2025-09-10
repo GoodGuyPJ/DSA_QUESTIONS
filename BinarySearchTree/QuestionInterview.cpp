c485183e5d5d395c6a7b0c458bd2354fa4661aad
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-09-10 23:18:14 +0530

    BST Height of BST

--- BinarySearchTree/QuestionInterview.cpp
+++ BinarySearchTree/QuestionInterview.cpp
@@ -1,9 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
- 
-int main(){
-    
- 
-  return 0;
+//Height of Binary Search Tree
+
+int height(struct Node* node){
+  if(node == nullptr){
+    return 0;
+  }
+  int left = height(node->left);
+  int right = height(node->right);
+  int ans = max(left, right) +1;
+
+  return ans;
 }
+
+//Diameter of Tree
